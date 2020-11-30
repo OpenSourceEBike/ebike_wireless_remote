2b2217819ea478bdf0fd776347540109fd938481
rananna <rnorth@kitavar.com>
2020-11-30 09:10:56 -0500

    brake flag, config led display and DFU

--- firmware/include/eeprom.h
+++ firmware/include/eeprom.h
@@ -16,9 +16,10 @@ typedef struct configurations
     uint32_t ui8_ant_device_id;
     uint32_t ui8_ant_lev_flag;
     uint32_t ui8_ant_controls_flag;
+    uint32_t ui8_brake_flag;
 } configurations_t;
 
-void eeprom_init(uint8_t *ant_num, uint8_t *bluetooth,uint8_t *ebike,uint8_t *garmin);
-void eeprom_write_variables(uint8_t ant_num, uint8_t bluetooth,uint8_t ebike,uint8_t garmin);
+void eeprom_init(uint8_t *ant_num, uint8_t *bluetooth,uint8_t *ebike,uint8_t *garmin,uint8_t *brake);
+void eeprom_write_variables(uint8_t ant_num, uint8_t bluetooth,uint8_t ebike,uint8_t garmin,uint8_t brake);
 
 #endif /* _EEPROM_H_ */
